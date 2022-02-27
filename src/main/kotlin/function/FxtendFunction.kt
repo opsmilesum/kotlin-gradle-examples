d5e5aba061689cb3cfe94616bc275cd33849d00c
Peng Ou <2896371938@qq.com>
2022-02-27 19:15:18 +0800

    Migrate build.gradle from Groovey to Kotlin DSL

--- src/main/kotlin/function/FxtendFunction.kt
+++ src/main/kotlin/function/FxtendFunction.kt
@@ -3,6 +3,6 @@ package function
 fun String.extendFunction(): String = this.substring(1, this.length - 1)
 
 fun main() {
-    val str = "XHello, WorldX"
+    val str = "XHello, The WorldX"
     println(str.extendFunction())
 }
